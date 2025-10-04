de4a7ed5998c2da9560b7f57c27b16839af204bf
Valentin Breuß <vbreuss@gmail.com>
2025-10-04 22:43:22 +0200

    Fix review issue

--- Source/Mockolate.SourceGenerators/MockGenerator.cs
+++ Source/Mockolate.SourceGenerators/MockGenerator.cs
@@ -112,16 +112,18 @@ public class MockGenerator : IIncrementalGenerator
 				}
 				result.Add((actualName, mockToGenerate));
 			}
-			foreach (var item in mockToGenerate.AdditionalImplementations
-				.Where(item => classNames.Add((item.Namespace, item.ClassName))))
+			foreach (var item in mockToGenerate.AdditionalImplementations)
 			{
-				int suffix = 1;
-				var actualName = item.GetClassNameWithoutDots();
-				while (result.Any(r => r.Name == actualName))
+				if (classNames.Add((item.Namespace, item.ClassName)))
 				{
-					actualName = $"{item.GetClassNameWithoutDots()}_{suffix++}";
+					int suffix = 1;
+					var actualName = item.GetClassNameWithoutDots();
+					while (result.Any(r => r.Name == actualName))
+					{
+						actualName = $"{item.GetClassNameWithoutDots()}_{suffix++}";
+					}
+					result.Add((actualName, item));
 				}
-				result.Add((actualName, item));
 			}
 		}
 
