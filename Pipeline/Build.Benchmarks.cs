Mockerade/Mockerade

refactor!: Rename to Mockolate

Pipeline/Build.Benchmarks.cs
@@ -22,13 +22,13 @@ partial class Build
 			benchmarkDirectory.CreateOrCleanDirectory();
 
 			DotNetBuild(s => s
-				.SetProjectFile(Solution.Benchmarks.Mockerade_Benchmarks)
+				.SetProjectFile(Solution.Benchmarks.Mockolate_Benchmarks)
 				.SetConfiguration(Configuration.Release)
 				.EnableNoLogo());
 
 			DotNet(
-				$"{Solution.Benchmarks.Mockerade_Benchmarks.Name}.dll --exporters json --filter * --artifacts \"{benchmarkDirectory}\"",
-				Solution.Benchmarks.Mockerade_Benchmarks.Directory / "bin" / "Release");
+				$"{Solution.Benchmarks.Mockolate_Benchmarks.Name}.dll --exporters json --filter * --artifacts \"{benchmarkDirectory}\"",
+				Solution.Benchmarks.Mockolate_Benchmarks.Directory / "bin" / "Release");
 		});
 
 	Target BenchmarkResult => _ => _
@@ -36,14 +36,14 @@ partial class Build
 		.Executes(async () =>
 		{
 			if (!File.Exists(ArtifactsDirectory / "Benchmarks" / "results" /
-															 "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md"))
+															 "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md"))
 			{
 				Log.Information("Skip benchmark result, because no report file was generated.");
 				return;
 			}
 
 			string fileContent = await File.ReadAllTextAsync(ArtifactsDirectory / "Benchmarks" / "results" /
-			                                                 "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md");
+			                                                 "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md");
 			Log.Information("Report:\n {FileContent}", fileContent);
 			if (GitHubActions?.IsPullRequest == true)
 			{
@@ -56,7 +56,7 @@ partial class Build
 		{
 			await "Benchmarks".DownloadArtifactTo(ArtifactsDirectory, GithubToken);
 			if (!File.Exists(ArtifactsDirectory / "Benchmarks" / "results" /
-															 "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md"))
+															 "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md"))
 			{
 				Log.Information("Skip benchmark comment, because no report file was generated.");
 				return;
@@ -77,7 +77,7 @@ partial class Build
 				Credentials tokenAuth = new(GithubToken);
 				gitHubClient.Credentials = tokenAuth;
 				IReadOnlyList<IssueComment> comments =
-					await gitHubClient.Issue.Comment.GetAllForIssue("Mockerade", "Mockerade", prId);
+					await gitHubClient.Issue.Comment.GetAllForIssue("Mockolate", "Mockolate", prId);
 				long? commentId = null;
 				Log.Information($"Found {comments.Count} comments");
 				foreach (IssueComment comment in comments)
@@ -92,12 +92,12 @@ partial class Build
 				if (commentId == null)
 				{
 					Log.Information($"Create comment:\n{body}");
-					await gitHubClient.Issue.Comment.Create("Mockerade", "Mockerade", prId, body);
+					await gitHubClient.Issue.Comment.Create("Mockolate", "Mockolate", prId, body);
 				}
 				else
 				{
 					Log.Information($"Update comment:\n{body}");
-					await gitHubClient.Issue.Comment.Update("Mockerade", "Mockerade", commentId.Value, body);
+					await gitHubClient.Issue.Comment.Update("Mockolate", "Mockolate", commentId.Value, body);
 				}
 			}
 		});
@@ -109,7 +109,7 @@ partial class Build
 	string CreateBenchmarkCommentBody()
 	{
 		string[] fileContent = File.ReadAllLines(ArtifactsDirectory / "Benchmarks" / "results" /
-		                                         "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md");
+		                                         "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md");
 		StringBuilder sb = new();
 		sb.AppendLine("## :rocket: Benchmark Results");
 		sb.AppendLine("<details>");
@@ -134,7 +134,7 @@ partial class Build
 				continue;
 			}
 
-			if (line.StartsWith('|') && line.Contains("_Mockerade", StringComparison.OrdinalIgnoreCase) && line.EndsWith('|'))
+			if (line.StartsWith('|') && line.Contains("_Mockolate", StringComparison.OrdinalIgnoreCase) && line.EndsWith('|'))
 			{
 				MakeLineBold(sb, line);
 				continue;

Pipeline/Build.CodeAnalysis.cs
@@ -17,7 +17,7 @@ partial class Build
 		{
 			SonarScannerTasks.SonarScannerBegin(s => s
 				.SetOrganization("mockerade")
-				.SetProjectKey("Mockerade_Mockerade")
+				.SetProjectKey("Mockolate_Mockolate")
 				.AddVSTestReports(TestResultsDirectory / "*.trx")
 				.AddOpenCoverPaths(TestResultsDirectory / "reports" / "OpenCover.xml")
 				.SetPullRequestOrBranchName(GitHubActions, GitVersion)

Pipeline/Build.MutationTests.cs
@@ -45,7 +45,7 @@ partial class Build
 			Dictionary<Project, Project[]> projects = new()
 			{
 				{
-					Solution.Mockerade, [Solution.Tests.Mockerade_Tests,]
+					Solution.Mockolate, [Solution.Tests.Mockolate_Tests,]
 				},
 			};
 
@@ -64,7 +64,7 @@ partial class Build
 				                      {
 				                      	"stryker-config": {
 				                      		"project-info": {
-				                      			"name": "github.com/Mockerade/Mockerade",
+				                      			"name": "github.com/Mockolate/Mockolate",
 				                      			"module": "{{project.Key.Name}}",
 				                      			"version": "{{branchName}}"
 				                      		},
@@ -137,7 +137,7 @@ partial class Build
 			Dictionary<Project, Project[]> projects = new()
 			{
 				{
-					Solution.Mockerade, [Solution.Tests.Mockerade_Tests,]
+					Solution.Mockolate, [Solution.Tests.Mockolate_Tests,]
 				},
 			};
 
@@ -152,7 +152,7 @@ partial class Build
 					Credentials tokenAuth = new(GithubToken);
 					gitHubClient.Credentials = tokenAuth;
 					IReadOnlyList<IssueComment> comments =
-						await gitHubClient.Issue.Comment.GetAllForIssue("Mockerade", "Mockerade", prId);
+						await gitHubClient.Issue.Comment.GetAllForIssue("Mockolate", "Mockolate", prId);
 					long? commentId = null;
 					Log.Information($"Found {comments.Count} comments");
 					foreach (IssueComment comment in comments)
@@ -167,12 +167,12 @@ partial class Build
 					if (commentId == null)
 					{
 						Log.Information($"Create comment:\n{body}");
-						await gitHubClient.Issue.Comment.Create("Mockerade", "Mockerade", prId, body);
+						await gitHubClient.Issue.Comment.Create("Mockolate", "Mockolate", prId, body);
 					}
 					else
 					{
 						Log.Information($"Update comment:\n{body}");
-						await gitHubClient.Issue.Comment.Update("Mockerade", "Mockerade", commentId.Value, body);
+						await gitHubClient.Issue.Comment.Update("Mockolate", "Mockolate", commentId.Value, body);
 					}
 				}
 			}

Pipeline/Build.Pack.cs
@@ -29,10 +29,10 @@ partial class Build
 			string[] lines = File.ReadAllLines(Solution.Directory / "README.md");
 			sb.AppendLine(lines.First());
 			sb.AppendLine(
-				$"[![Changelog](https://img.shields.io/badge/Changelog-v{version}-blue)](https://github.com/Mockerade/Mockerade/releases/tag/v{version})");
+				$"[![Changelog](https://img.shields.io/badge/Changelog-v{version}-blue)](https://github.com/Mockolate/Mockolate/releases/tag/v{version})");
 			foreach (string line in lines.Skip(1))
 			{
-				if (line.StartsWith("[![Build](https://github.com/Mockerade/Mockerade/actions/workflows/build.yml") ||
+				if (line.StartsWith("[![Build](https://github.com/Mockolate/Mockolate/actions/workflows/build.yml") ||
 				    line.StartsWith("[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure"))
 				{
 					continue;
@@ -70,7 +70,7 @@ partial class Build
 			List<string> packages = new();
 			foreach (Project project in new[]
 			         {
-				         Solution.Mockerade,
+				         Solution.Mockolate,
 			         })
 			{
 				foreach (string package in

Pipeline/BuildExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
@@ -50,10 +50,10 @@ public static class BuildExtensions
 		}
 
 		using HttpClient client = new();
-		client.DefaultRequestHeaders.UserAgent.ParseAdd("Mockerade");
+		client.DefaultRequestHeaders.UserAgent.ParseAdd("Mockolate");
 		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", githubToken);
 		HttpResponseMessage response = await client.GetAsync(
-			$"https://api.github.com/repos/Mockerade/Mockerade/actions/runs/{runId}/artifacts");
+			$"https://api.github.com/repos/Mockolate/Mockolate/actions/runs/{runId}/artifacts");
 
 		string responseContent = await response.Content.ReadAsStringAsync();
 		if (!response.IsSuccessStatusCode)
@@ -72,7 +72,7 @@ public static class BuildExtensions
 				{
 					long artifactId = artifact.GetProperty("id").GetInt64();
 					HttpResponseMessage fileResponse = await client.GetAsync(
-						$"https://api.github.com/repos/Mockerade/Mockerade/actions/artifacts/{artifactId}/zip");
+						$"https://api.github.com/repos/Mockolate/Mockolate/actions/artifacts/{artifactId}/zip");
 					if (fileResponse.IsSuccessStatusCode)
 					{
 						using ZipArchive archive = new(await fileResponse.Content.ReadAsStreamAsync());

aweXpect/Mockolate

Pipeline/Build.Benchmarks.cs
@@ -22,13 +22,13 @@ partial class Build
 			benchmarkDirectory.CreateOrCleanDirectory();
 
 			DotNetBuild(s => s
-				.SetProjectFile(Solution.Benchmarks.Mockerade_Benchmarks)
+				.SetProjectFile(Solution.Benchmarks.Mockolate_Benchmarks)
 				.SetConfiguration(Configuration.Release)
 				.EnableNoLogo());
 
 			DotNet(
-				$"{Solution.Benchmarks.Mockerade_Benchmarks.Name}.dll --exporters json --filter * --artifacts \"{benchmarkDirectory}\"",
-				Solution.Benchmarks.Mockerade_Benchmarks.Directory / "bin" / "Release");
+				$"{Solution.Benchmarks.Mockolate_Benchmarks.Name}.dll --exporters json --filter * --artifacts \"{benchmarkDirectory}\"",
+				Solution.Benchmarks.Mockolate_Benchmarks.Directory / "bin" / "Release");
 		});
 
 	Target BenchmarkResult => _ => _
@@ -36,14 +36,14 @@ partial class Build
 		.Executes(async () =>
 		{
 			if (!File.Exists(ArtifactsDirectory / "Benchmarks" / "results" /
-															 "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md"))
+															 "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md"))
 			{
 				Log.Information("Skip benchmark result, because no report file was generated.");
 				return;
 			}
 
 			string fileContent = await File.ReadAllTextAsync(ArtifactsDirectory / "Benchmarks" / "results" /
-			                                                 "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md");
+			                                                 "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md");
 			Log.Information("Report:\n {FileContent}", fileContent);
 			if (GitHubActions?.IsPullRequest == true)
 			{
@@ -56,7 +56,7 @@ partial class Build
 		{
 			await "Benchmarks".DownloadArtifactTo(ArtifactsDirectory, GithubToken);
 			if (!File.Exists(ArtifactsDirectory / "Benchmarks" / "results" /
-															 "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md"))
+															 "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md"))
 			{
 				Log.Information("Skip benchmark comment, because no report file was generated.");
 				return;
@@ -77,7 +77,7 @@ partial class Build
 				Credentials tokenAuth = new(GithubToken);
 				gitHubClient.Credentials = tokenAuth;
 				IReadOnlyList<IssueComment> comments =
-					await gitHubClient.Issue.Comment.GetAllForIssue("Mockerade", "Mockerade", prId);
+					await gitHubClient.Issue.Comment.GetAllForIssue("aweXpect", "Mockolate", prId);
 				long? commentId = null;
 				Log.Information($"Found {comments.Count} comments");
 				foreach (IssueComment comment in comments)
@@ -92,12 +92,12 @@ partial class Build
 				if (commentId == null)
 				{
 					Log.Information($"Create comment:\n{body}");
-					await gitHubClient.Issue.Comment.Create("Mockerade", "Mockerade", prId, body);
+					await gitHubClient.Issue.Comment.Create("aweXpect", "Mockolate", prId, body);
 				}
 				else
 				{
 					Log.Information($"Update comment:\n{body}");
-					await gitHubClient.Issue.Comment.Update("Mockerade", "Mockerade", commentId.Value, body);
+					await gitHubClient.Issue.Comment.Update("aweXpect", "Mockolate", commentId.Value, body);
 				}
 			}
 		});
@@ -109,7 +109,7 @@ partial class Build
 	string CreateBenchmarkCommentBody()
 	{
 		string[] fileContent = File.ReadAllLines(ArtifactsDirectory / "Benchmarks" / "results" /
-		                                         "Mockerade.Benchmarks.HappyCaseBenchmarks-report-github.md");
+		                                         "Mockolate.Benchmarks.HappyCaseBenchmarks-report-github.md");
 		StringBuilder sb = new();
 		sb.AppendLine("## :rocket: Benchmark Results");
 		sb.AppendLine("<details>");
@@ -134,7 +134,7 @@ partial class Build
 				continue;
 			}
 
-			if (line.StartsWith('|') && line.Contains("_Mockerade", StringComparison.OrdinalIgnoreCase) && line.EndsWith('|'))
+			if (line.StartsWith('|') && line.Contains("_Mockolate", StringComparison.OrdinalIgnoreCase) && line.EndsWith('|'))
 			{
 				MakeLineBold(sb, line);
 				continue;

Pipeline/Build.CodeAnalysis.cs
@@ -17,7 +17,7 @@ partial class Build
 		{
 			SonarScannerTasks.SonarScannerBegin(s => s
 				.SetOrganization("mockerade")
-				.SetProjectKey("Mockerade_Mockerade")
+				.SetProjectKey("aweXpect_Mockolate")
 				.AddVSTestReports(TestResultsDirectory / "*.trx")
 				.AddOpenCoverPaths(TestResultsDirectory / "reports" / "OpenCover.xml")
 				.SetPullRequestOrBranchName(GitHubActions, GitVersion)

Pipeline/Build.MutationTests.cs
@@ -45,7 +45,7 @@ partial class Build
 			Dictionary<Project, Project[]> projects = new()
 			{
 				{
-					Solution.Mockerade, [Solution.Tests.Mockerade_Tests,]
+					Solution.Mockolate, [Solution.Tests.Mockolate_Tests,]
 				},
 			};
 
@@ -64,7 +64,7 @@ partial class Build
 				                      {
 				                      	"stryker-config": {
 				                      		"project-info": {
-				                      			"name": "github.com/Mockerade/Mockerade",
+				                      			"name": "github.com/aweXpect/Mockolate",
 				                      			"module": "{{project.Key.Name}}",
 				                      			"version": "{{branchName}}"
 				                      		},
@@ -137,7 +137,7 @@ partial class Build
 			Dictionary<Project, Project[]> projects = new()
 			{
 				{
-					Solution.Mockerade, [Solution.Tests.Mockerade_Tests,]
+					Solution.Mockolate, [Solution.Tests.Mockolate_Tests,]
 				},
 			};
 
@@ -152,7 +152,7 @@ partial class Build
 					Credentials tokenAuth = new(GithubToken);
 					gitHubClient.Credentials = tokenAuth;
 					IReadOnlyList<IssueComment> comments =
-						await gitHubClient.Issue.Comment.GetAllForIssue("Mockerade", "Mockerade", prId);
+						await gitHubClient.Issue.Comment.GetAllForIssue("aweXpect", "Mockolate", prId);
 					long? commentId = null;
 					Log.Information($"Found {comments.Count} comments");
 					foreach (IssueComment comment in comments)
@@ -167,12 +167,12 @@ partial class Build
 					if (commentId == null)
 					{
 						Log.Information($"Create comment:\n{body}");
-						await gitHubClient.Issue.Comment.Create("Mockerade", "Mockerade", prId, body);
+						await gitHubClient.Issue.Comment.Create("aweXpect", "Mockolate", prId, body);
 					}
 					else
 					{
 						Log.Information($"Update comment:\n{body}");
-						await gitHubClient.Issue.Comment.Update("Mockerade", "Mockerade", commentId.Value, body);
+						await gitHubClient.Issue.Comment.Update("aweXpect", "Mockolate", commentId.Value, body);
 					}
 				}
 			}

Pipeline/Build.Pack.cs
@@ -29,10 +29,10 @@ partial class Build
 			string[] lines = File.ReadAllLines(Solution.Directory / "README.md");
 			sb.AppendLine(lines.First());
 			sb.AppendLine(
-				$"[![Changelog](https://img.shields.io/badge/Changelog-v{version}-blue)](https://github.com/Mockerade/Mockerade/releases/tag/v{version})");
+				$"[![Changelog](https://img.shields.io/badge/Changelog-v{version}-blue)](https://github.com/aweXpect/Mockolate/releases/tag/v{version})");
 			foreach (string line in lines.Skip(1))
 			{
-				if (line.StartsWith("[![Build](https://github.com/Mockerade/Mockerade/actions/workflows/build.yml") ||
+				if (line.StartsWith("[![Build](https://github.com/aweXpect/Mockolate/actions/workflows/build.yml") ||
 				    line.StartsWith("[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure"))
 				{
 					continue;
@@ -70,7 +70,7 @@ partial class Build
 			List<string> packages = new();
 			foreach (Project project in new[]
 			         {
-				         Solution.Mockerade,
+				         Solution.Mockolate,
 			         })
 			{
 				foreach (string package in

Pipeline/BuildExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO.Compression;
 using System.Linq;
 using System.Net.Http;
@@ -50,10 +50,10 @@ public static class BuildExtensions
 		}
 
 		using HttpClient client = new();
-		client.DefaultRequestHeaders.UserAgent.ParseAdd("Mockerade");
+		client.DefaultRequestHeaders.UserAgent.ParseAdd("Mockolate");
 		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", githubToken);
 		HttpResponseMessage response = await client.GetAsync(
-			$"https://api.github.com/repos/Mockerade/Mockerade/actions/runs/{runId}/artifacts");
+			$"https://api.github.com/repos/aweXpect/Mockolate/actions/runs/{runId}/artifacts");
 
 		string responseContent = await response.Content.ReadAsStringAsync();
 		if (!response.IsSuccessStatusCode)
@@ -72,7 +72,7 @@ public static class BuildExtensions
 				{
 					long artifactId = artifact.GetProperty("id").GetInt64();
 					HttpResponseMessage fileResponse = await client.GetAsync(
-						$"https://api.github.com/repos/Mockerade/Mockerade/actions/artifacts/{artifactId}/zip");
+						$"https://api.github.com/repos/aweXpect/Mockolate/actions/artifacts/{artifactId}/zip");
 					if (fileResponse.IsSuccessStatusCode)
 					{
 						using ZipArchive archive = new(await fileResponse.Content.ReadAsStreamAsync());