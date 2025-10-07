db1baeb6281ef9a7c0bc96573c312a73f443feed
Valentin Breuß <vbreuss@gmail.com>
2025-10-07 17:34:25 +0200

    Add tests for mock factory

---
+++ Tests/Mockolate.Tests/MockTests.Factory.cs
@@ -0,0 +1,21 @@
+using Mockolate.Exceptions;
+using Mockolate.Tests.TestHelpers;
+using static Mockolate.BaseClass;
+
+namespace Mockolate.Tests;
+
+public sealed partial class MockTests
+{
+	[Fact]
+	public async Task Factory_ShouldUseDefinedBehavior()
+	{
+		var behavior = MockBehavior.Default with { ThrowWhenNotSetup = true };
+		var factory = new Mock.Factory(behavior);
+
+		var mock1 = factory.Create<IMyService>();
+		var mock2 = factory.Create<MyServiceBase, IMyService>();
+
+		await That(((IMock)mock1).Behavior).IsSameAs(behavior);
+		await That(((IMock)mock2).Behavior).IsSameAs(behavior);
+	}
+}
