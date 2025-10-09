c85f676b0e4cf8e5736ea602820a56abba167ead
Valentin Breuß <vbreuss@gmail.com>
2025-10-09 08:14:14 +0200

    Use Task.Delay instead of yield

--- Tests/Mockolate.Tests/Interactions/MockInteractionsTests.cs
+++ Tests/Mockolate.Tests/Interactions/MockInteractionsTests.cs
@@ -14,18 +14,18 @@ public sealed class MockInteractionsTests
 		ConcurrentQueue<int> retrievedIds = [];
 		for (int i = 0; i < 50; i++)
 		{
-			tasks[i] = Task.Run(() =>
+			tasks[i] = Task.Run(async () =>
 			{
-				for (int i = 0; i < 50; i++)
+				for (int i = 0; i < 20; i++)
 				{
 					retrievedIds.Enqueue(sut.GetNextIndex());
-					Task.Yield();
+					await Task.Delay(1);
 				}
 			}, cancellationToken: CancellationToken.None);
 		}
 		await Task.WhenAll(tasks);
 
-		await That(retrievedIds.Count).IsEqualTo(2500);
+		await That(retrievedIds.Count).IsEqualTo(1000);
 		await That(retrievedIds).AreAllUnique();
 	}
 
@@ -37,19 +37,19 @@ public sealed class MockInteractionsTests
 		var tasks = new Task[50];
 		for (int i = 0; i < 50; i++)
 		{
-			tasks[i] = Task.Run(() =>
+			tasks[i] = Task.Run(async () =>
 			{
-				for (int i = 0; i < 50; i++)
+				for (int i = 0; i < 20; i++)
 				{
 					var index = sut.GetNextIndex();
 					interactions.RegisterInteraction(new PropertySetterAccess(index, "MyTestProperty", index));
-					Task.Yield();
+					await Task.Delay(1);
 				}
 			}, cancellationToken: CancellationToken.None);
 		}
 		await Task.WhenAll(tasks);
 
-		await That(sut.Count).IsEqualTo(2500);
+		await That(sut.Count).IsEqualTo(1000);
 		await That(sut.Interactions).IsInAscendingOrder(x => x.Index);
 		await That(sut.Interactions).AreAllUnique();
 	}
