fb8ec75827e8f892066189c4a5ce55237fbdcb2a
Valentin Breuß <vbreuss@gmail.com>
2025-11-21 15:57:52 +0100

    Use --project

--- Pipeline/Build.UnitTest.cs
+++ Pipeline/Build.UnitTest.cs
@@ -39,11 +39,12 @@ partial class Build
 					.CombineWith(
 						UnitTestProjects,
 						(settings, project) => settings
-							.SetProjectFile(project)
 							.CombineWith(
 								project.GetTargetFrameworks()?.Except(excludedFrameworks),
 								(frameworkSettings, framework) => frameworkSettings
 									.SetFramework(framework)
+									.AddProcessAdditionalArguments(
+										$"--project \"{project.Path}\"")
 									.AddLoggers($"console;verbosity=normal;trx;LogFileName={project.Name}_{framework}.trx")
 							)
 					), completeOnFailure: true
