123190d2f066cb274316ac33839fd1a18d6041e0
Valentin Breuß <vbreuss@gmail.com>
2025-11-25 22:35:03 +0100

    feat: add `ThrowingWhenNotSetup` extension method (#263)
    This PR introduces a new `ThrowingWhenNotSetup` extension method for `MockBehavior`, allowing users to configure whether mocks should throw exceptions when operations are attempted without prior setup. The implementation follows the existing pattern established by `CallingBaseClass` and leverages C# 10's extension method syntax.
    
    ### Key Changes:
    - Added `ThrowingWhenNotSetup` extension method to `MockBehaviorExtensions`
    - Refactored to use C# 10 extension syntax for cleaner code organization
    - Added comprehensive test coverage for the new method

--- Source/Mockolate/MockBehaviorExtensions.cs
+++ Source/Mockolate/MockBehaviorExtensions.cs
@@ -5,16 +5,32 @@ namespace Mockolate;
 /// </summary>
 public static class MockBehaviorExtensions
 {
-	/// <summary>
-	///     Specifies if the base class implementation should be called, and
-	///     its return values used as default values.
-	/// </summary>
-	/// <remarks>
-	///     Sets the <see cref="MockBehavior.CallBaseClass" /> to <paramref name="callBaseClass" />.
-	/// </remarks>
-	public static MockBehavior CallingBaseClass(this MockBehavior mockBehavior, bool callBaseClass = true)
-		=> mockBehavior with
-		{
-			CallBaseClass = callBaseClass,
-		};
+	/// <inheritdoc cref="MockBehaviorExtensions" />
+	extension(MockBehavior mockBehavior)
+	{
+		/// <summary>
+		///     Specifies if the base class implementation should be called, and
+		///     its return values used as default values.
+		/// </summary>
+		/// <remarks>
+		///     Sets the <see cref="MockBehavior.CallBaseClass" /> to <paramref name="callBaseClass" />.
+		/// </remarks>
+		public MockBehavior CallingBaseClass(bool callBaseClass = true)
+			=> mockBehavior with
+			{
+				CallBaseClass = callBaseClass,
+			};
+
+		/// <summary>
+		///     Specifies whether an exception is thrown when an operation is attempted without prior setup.
+		/// </summary>
+		/// <remarks>
+		///     Sets the <see cref="MockBehavior.ThrowWhenNotSetup" /> to <paramref name="throwWhenNotSetup" />.
+		/// </remarks>
+		public MockBehavior ThrowingWhenNotSetup(bool throwWhenNotSetup = true)
+			=> mockBehavior with
+			{
+				ThrowWhenNotSetup = throwWhenNotSetup,
+			};
+	}
 }
