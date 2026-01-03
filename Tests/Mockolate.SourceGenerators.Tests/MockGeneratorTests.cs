03504d7e81d000e31f709c808136bdbe9c25773c
Valentin Breuß <vbreuss@gmail.com>
2026-01-03 20:29:00 +0100

    feat: initial support for mocking `HttpClient`

--- Tests/Mockolate.SourceGenerators.Tests/MockGeneratorTests.cs
+++ Tests/Mockolate.SourceGenerators.Tests/MockGeneratorTests.cs
@@ -394,4 +394,31 @@ public class MockGeneratorTests
 		await That(result.Sources).ContainsKey("MockForIMyInterface.g.cs");
 		await That(result.Sources).ContainsKey("MockForIMyInterfaceExtensions.g.cs");
 	}
+
+	[Fact]
+	public async Task WithHttpClient_ShouldAlsoGenerateMockForHttpMessageHandler()
+	{
+		GeneratorResult result = Generator
+			.Run("""
+			     using System;
+			     using System.Net.Http;
+			     using System.Threading;
+			     using System.Threading.Tasks;
+			     using Mockolate;
+			     namespace MyCode
+			     {
+			         public class Program
+			         {
+			             public static void Main(string[] args)
+			             {
+			     			_ = Mock.Create<HttpClient>();
+			             }
+			         }
+			     }
+			     """, typeof(HttpClient));
+
+		await That(result.Diagnostics).IsEmpty();
+		await That(result.Sources).ContainsKey("MockForHttpMessageHandler.g.cs").And
+			.ContainsKey("MockForHttpClient.g.cs");
+	}
 }
