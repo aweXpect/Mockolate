aacf9f2c5db669c1f164057c3ba09042a5b9941f
Valentin Breuß <vbreuss@gmail.com>
2026-01-16 14:59:28 +0100

    Add additional tests

--- Tests/Mockolate.Tests/Web/HttpClientExtensionsTests.cs
+++ Tests/Mockolate.Tests/Web/HttpClientExtensionsTests.cs
@@ -1,3 +1,50 @@
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using Mockolate.Interactions;
+using Mockolate.Parameters;
+using Mockolate.Setup;
+using Mockolate.Web;
+
 namespace Mockolate.Tests.Web;
 
-public sealed partial class HttpClientExtensionsTests;
+public sealed partial class HttpClientExtensionsTests
+{
+	[Fact]
+	public async Task InvalidParameter_ShouldReturnTrue()
+	{
+		HttpClient httpClient = Mock.Create<HttpClient>();
+		IReturnMethodSetup<Task<HttpResponseMessage>, HttpRequestMessage, CancellationToken> setup = httpClient
+			.SetupMock.Method
+			.GetAsync(new InvalidParameter())
+			.ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK));
+		IInteractiveMethodSetup interactiveSetup = (IInteractiveMethodSetup)setup;
+
+		bool result = interactiveSetup.Matches(new MethodInvocation(0, "System.Net.Http.HttpMessageHandler.SendAsync",
+			[new HttpRequestMessage(), CancellationToken.None,]));
+
+		await That(result).IsTrue();
+	}
+
+	[Fact]
+	public async Task NullUri_ShouldReturnFalse()
+	{
+		HttpClient httpClient = Mock.Create<HttpClient>();
+		IReturnMethodSetup<Task<HttpResponseMessage>, HttpRequestMessage, CancellationToken> setup = httpClient
+			.SetupMock.Method
+			.GetAsync(It.IsAny<string?>())
+			.ReturnsAsync(new HttpResponseMessage(HttpStatusCode.OK));
+		IInteractiveMethodSetup interactiveSetup = (IInteractiveMethodSetup)setup;
+
+		bool result = interactiveSetup.Matches(new MethodInvocation(0, "System.Net.Http.HttpMessageHandler.SendAsync",
+			[new HttpRequestMessage(), CancellationToken.None,]));
+
+		await That(result).IsFalse();
+	}
+
+	private sealed class InvalidParameter : IParameter<string?>
+	{
+		public IParameter<string?> Do(Action<string?> callback)
+			=> throw new NotSupportedException();
+	}
+}
