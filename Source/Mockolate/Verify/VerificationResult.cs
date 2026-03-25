f4b38d892f2ff40e8a737b5229b5fefa6f2d30de
Breuß Valentin <valentin.breuss@baur.eu>
2026-03-25 15:21:50 +0100

    Revert API changes

--- Source/Mockolate/Verify/VerificationResult.cs
+++ Source/Mockolate/Verify/VerificationResult.cs
@@ -19,6 +19,18 @@ public class VerificationResult<TVerify> : IVerificationResult<TVerify>, IVerifi
 	private readonly Func<IInteraction, bool> _predicate;
 	private readonly TVerify _verify;
 
+	/// <inheritdoc cref="VerificationResult{TMock}" />
+	public VerificationResult(TVerify verify,
+		MockInteractions interactions,
+		Func<IInteraction, bool> predicate,
+		string expectation)
+	{
+		_verify = verify;
+		_interactions = interactions;
+		_predicate = predicate;
+		_expectationFactory = () => expectation;
+	}
+
 	internal VerificationResult(TVerify verify,
 		MockInteractions interactions,
 		Func<IInteraction, bool> predicate,
