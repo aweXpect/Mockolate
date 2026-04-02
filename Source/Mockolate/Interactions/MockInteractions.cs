d2af7146a18836cc681be5722f2ddcd4ddc822d2
Valentin Breuß <vbreuss@gmail.com>
2026-04-02 21:09:14 +0200

    Fix review issues

--- Source/Mockolate/Interactions/MockInteractions.cs
+++ Source/Mockolate/Interactions/MockInteractions.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+#if NET10_0_OR_GREATER
+using System.Threading;
+#endif
 
 namespace Mockolate.Interactions;
 
@@ -11,14 +14,40 @@ namespace Mockolate.Interactions;
 /// </summary>
 [DebuggerDisplay("{_interactions.Count} interactions")]
 [DebuggerNonUserCode]
-public class MockInteractions : IEnumerable<IInteraction>, IMockInteractions
+public class MockInteractions : IReadOnlyCollection<IInteraction>, IMockInteractions
 {
 	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
 	private readonly List<IInteraction> _interactions = [];
+
 	[DebuggerBrowsable(DebuggerBrowsableState.Never)]
+#if NET10_0_OR_GREATER
+	private readonly Lock _lock = new();
+#else
 	private readonly object _lock = new();
+#endif
+
 	private int _index = -1;
 	private List<IInteraction>? _missingVerification;
+
+	/// <inheritdoc cref="IMockInteractions.RegisterInteraction{TInteraction}(TInteraction)" />
+	TInteraction IMockInteractions.RegisterInteraction<TInteraction>(TInteraction interaction)
+	{
+		if (interaction is not ISettableInteraction settableInteraction)
+		{
+			// ReSharper disable once LocalizableElement
+			throw new ArgumentException("Only settable interactions can be registered.", nameof(interaction));
+		}
+
+		lock (_lock)
+		{
+			_missingVerification?.Add(interaction);
+			settableInteraction.SetIndex(++_index);
+			_interactions.Add(interaction);
+		}
+
+		InteractionAdded?.Invoke(this, EventArgs.Empty);
+		return interaction;
+	}
 
 	/// <summary>
 	///     The number of interactions contained in the collection.
@@ -36,30 +65,16 @@ public class MockInteractions : IEnumerable<IInteraction>, IMockInteractions
 
 	/// <inheritdoc cref="IEnumerable{IInteraction}.GetEnumerator()" />
 	public IEnumerator<IInteraction> GetEnumerator()
-		=> _interactions.GetEnumerator();
+	{
+		lock (_lock)
+		{
+			return _interactions.ToList().GetEnumerator();
+		}
+	}
 
 	/// <inheritdoc cref="IEnumerable.GetEnumerator()" />
 	IEnumerator IEnumerable.GetEnumerator()
 		=> GetEnumerator();
-
-	/// <inheritdoc cref="IMockInteractions.RegisterInteraction{TInteraction}(TInteraction)" />
-	TInteraction IMockInteractions.RegisterInteraction<TInteraction>(TInteraction interaction)
-	{
-		if (interaction is not ISettableInteraction settableInteraction)
-		{
-			throw new ArgumentException("Only settable interactions can be registered.", nameof(interaction));
-		}
-
-		lock (_lock)
-		{
-			_missingVerification?.Add(interaction);
-			settableInteraction.SetIndex(++_index);
-			_interactions.Add(interaction);
-		}
-
-		InteractionAdded?.Invoke(this, EventArgs.Empty);
-		return interaction;
-	}
 
 	/// <summary>
 	///     Gets the unverified interactions of the mock.
