e92e944875466f155eca71027f4782d5d511228c
Breuß Valentin <valentin.breuss@baur.eu>
2026-04-26 06:46:21 +0200

    Cleanup Benchmarks and accept API changes

--- Benchmarks/Mockolate.Benchmarks/Program.cs
+++ Benchmarks/Mockolate.Benchmarks/Program.cs
@@ -1,12 +1,4 @@
 ﻿using BenchmarkDotNet.Running;
 using Mockolate.Benchmarks;
 
-if (args.Length == 1 && args[0] == "--smoke")
-{
-	SuggestedMocksSmokeTest.Run();
-	System.Console.WriteLine("smoke ok");
-	return;
-}
-
-SuggestedMocksSmokeTest.Run();
 BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
